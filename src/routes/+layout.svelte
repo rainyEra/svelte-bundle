300b528cc519bd1ff1170fa91062a8e5185834a5
CodeSandbox <support@codesandbox.io>
2022-11-25 22:01:19 +0000

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+
+	import './styles.css';
+</script>
+
+<div class="app">
+<p>Helldo!</p>
+</div>
+
+<style>
+
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,31 @@
+<script>
+
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section>
+	<h1>
+		<span class="welcome">
+			<picture>
+				<source srcset={welcome} type="image/webp" />
+				<img src={welcome_fallback} alt="Welcome" />
+			</picture>
+		</span>
+
+		to your new<br />SvelteKit app
+	</h1>
+
+	<h2>
+		try editing <strong>src/routes/+page.svelte</strong>
+	</h2>
+
+	<Counter />
+</section>
+
+<style>
+
+</style>
